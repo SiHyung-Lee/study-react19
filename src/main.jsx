a0c5aa9ef0a44c791f9f270ad5e9bdbb349a4048
SiHyung Lee <buzzcatfish@gmail.com>
2025-08-26 11:55:57 +0900

    feat(router): Add React Router for navigation
    - React Router와 React Router DOM을 추가하여 애플리케이션 내에서 페이지 간 탐색을 가능하게 함.
    - main.jsx 파일에서 BrowserRouter와 Routes를 설정하여 기본 경로와 회원가입 경로를 정의함.
    - package.json 및 package-lock.json에 필요한 의존성을 추가함.
    
    이 변경 사항은 애플리케이션의 라우팅 기능을 향상시킵니다.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import App from "./App.jsx";
+import App from "./App.jsx";
 import Signup from "./Signup.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Signup />
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path="/"
+        element={<App />}
+      />
+      <Route
+        path="/signup"
+        element={<Signup />}
+      />
+    </Routes>
+  </BrowserRouter>
 );
